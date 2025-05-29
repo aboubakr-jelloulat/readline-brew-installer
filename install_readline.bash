0ec626709fd286b56d1d4671604e3a3bb2f8fffc
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-29 10:12:52 +0100

    install GNU Readline

---
+++ install_readline.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+42 -c
+
+42 -brew
+
+42 -c
+
+brew install readline
+
+
+brew link readline --force
+
+brew --prefix readline
+
+#in the following export set you're path to the readline
+
+export LDFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/lib"
+export CPPFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/include"
+
+# export LDFLAGS="-L/goinfre/ajelloul/homebrew/opt/readline/lib"
+# export CPPFLAGS="-I/goinfre/ajelloul/homebrew/opt/readline/include"
+
+#for rerunning terminal
+source ~/.zshrc
+
+
