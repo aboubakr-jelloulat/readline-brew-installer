049ae4e8b77d91367bae29b5a3cce367c55fbc6e
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-06-05 13:42:04 +0100

    add wizzard brew

--- install_readline.bash
+++ install_readline.bash
@@ -2,7 +2,8 @@
 
 42 -c
 
-42 -brew
+42-wizzard-brew 
+
 
 42 -c
 
@@ -15,8 +16,11 @@ brew --prefix readline
 
 #in the following export set you're path to the readline
 
-export LDFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/lib"
-export CPPFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/include"
+# export LDFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/lib"
+# export CPPFLAGS="-I/Users/"ur username"/.brew/Cellar/readline/8.2.13/include"
+
+export LDFLAGS="-L/goinfre/ur login/homebrew/opt/readline/lib"
+export CPPFLAGS="-I/goinfre/ur login/homebrew/opt/readline/include"
 
 # export LDFLAGS="-L/goinfre/ajelloul/homebrew/opt/readline/lib"
 # export CPPFLAGS="-I/goinfre/ajelloul/homebrew/opt/readline/include"
